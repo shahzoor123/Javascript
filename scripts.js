33c82e413fed32df02488232b1cb846e0f28a59d
shahzor khan <shahzoorkhan321@gmail.com>
2023-03-10 14:52:28 +0500

    javascripts fundamental

---
+++ scripts.js
@@ -0,0 +1,144 @@
+// start
+
+
+    // let greet = document.querySelector('button');
+
+    // greet.addEventListener('click', addition);
+
+
+    // function inputMsg() {
+    //     let name  = prompt('enter your meter number');
+    //     greet.textContent = name
+        
+    // }
+
+
+
+
+
+// functions
+
+    // function addition() {
+    //     let a  = prompt('enter your meter number');
+    //     let b  = prompt('enter your meter number');
+    //     c = a+b
+    //     if(c > 1000){
+    //         greet.textContent = c
+    //     }
+    //     else{
+    //         greet.textContent = 'To low to print'
+    //     }
+        
+
+    // }
+
+// conditional statements
+
+
+     // let time = prompt("whats the time");
+        // if (time == 12 && time == 12 ){
+        //     let heading = document.querySelector('h1');
+        //     heading.textContent = time;
+        // }
+        // else{
+        //     let heading = document.querySelector('h1');
+        //     heading.textContent = 'Not the time yet';
+        // }
+        
+
+        // let heading = document.querySelector('h1');
+        // heading.textContent = 'Welcome to aperture car dealership Volvo && Tesla';
+
+        // let user_car = prompt('What type of car you want')
+
+
+
+
+// switch statements
+
+
+        // switch (user_car){
+        //     case 'volvo':
+        //     cars = document.querySelector('h2');
+        //     cars.textContent = 'we have volvo EX90 / electric 4 motor / 650hp';
+        //     break;
+
+        //     case 'tesla':
+        //     cars = document.querySelector('h2');
+        //     cars.textContent = 'we have tesla model x / electric 4 motor  / 800hp';
+        //     break;
+
+        //     default:
+        //     cars = document.querySelector('h2');
+        //     cars.textContent = (`${user_car} is not available right now but comming soon`);
+            
+
+        // }
+
+
+
+
+// for loop        
+        // for(let i = 0; i <= 100; i++){
+        //     let list = document.createElement('ul');
+        //     list.innerHTML = i;
+        //     document.body.appendChild(list)
+        
+            
+        // }
+
+
+// while loop
+
+        
+        // let x  = 0 ;
+        // while(x<=100){
+        //     console.log(`keep grinding ${x}`);
+        //     x++;
+        // }
+
+
+// do while loop
+
+// let i = 0;
+    // do {
+    //     console.log("hello");
+    //     console.log("hello");
+    //     console.log("hello");
+    //     console.log("hello");
+    //     console.log("hello");
+
+    //     i++;
+    // }
+    // while(i <= 10)
+
+
+// for in loop using objects
+
+    // let animal = {
+    //     name : "loin",
+    //     leg : 4,
+    //     name2 : "loi",
+    //     leg1 : 4,
+    // };
+   
+    // for (let key in animal){
+    //     console.log(key, animal[key])
+    // }
+
+
+
+// for in loop using objects
+    // let cars = ['f1', 'audi', 'bmw',]
+
+    // for(let car in cars){
+
+    //     console.log(cars[car])
+
+    // }
+
+// for of loop using arrays it will directly give you names instead of indexes
+
+    // for (let car of cars){
+    //     console.log(car)
+    // }    
